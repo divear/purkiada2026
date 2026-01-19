f89cb2c503b5424c8e55522864caef5d62d9a736
divear <lukyn.odehnal333@gmail.com>
2026-01-19 07:13:21 +0100

    start page

--- start_page.gd
+++ start_page.gd
@@ -1,18 +1,14 @@
 extends Control
 
 @export var start_button: Button
-@export var settings_button: Button
 
 func _ready() -> void:
 	# Automatically get buttons if not assigned
 	if start_button == null:
 		start_button = get_node("MainMenu/CenterContainer/MarginContainer/VBoxContainer/NewGameButton") as Button
-	if settings_button == null:
-		settings_button = get_node("MainMenu/CenterContainer/MarginContainer/VBoxContainer/SettingsButton") as Button
 
 	# Connect signals
 	start_button.pressed.connect(_on_start_pressed)
-	settings_button.pressed.connect(_on_settings_pressed)
 
 	print("Start Page Loaded")
 
